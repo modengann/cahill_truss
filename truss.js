2819a3e794832e3c1ae55e3dba3eb3d9f3e1f97e
Casey Moden <cmoden@gannacademy.org>
2026-04-29 12:05:23 -0400

    Task 2: add null guards to getMemberAngle, tighten tests

--- truss.js
+++ truss.js
@@ -1,8 +1,11 @@
 export function getMemberAngle(memberId, jointId, problem) {
   const member = problem.members.find(m => m.id === memberId);
+  if (!member) throw new Error(`getMemberAngle: member '${memberId}' not found`);
   const otherId = member.j1 === jointId ? member.j2 : member.j1;
   const from = problem.joints.find(j => j.id === jointId);
+  if (!from) throw new Error(`getMemberAngle: joint '${jointId}' not found`);
   const to   = problem.joints.find(j => j.id === otherId);
+  if (!to)   throw new Error(`getMemberAngle: joint '${otherId}' not found`);
   return Math.atan2(to.y - from.y, to.x - from.x);
 }
 
